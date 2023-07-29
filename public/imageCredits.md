8b1d1a890388792858f4aa0c6e367032cd4489bd
Deen-q <124711768+Deen-q@users.noreply.github.com>
2023-07-29 20:30:17 +0100

    Huge progress. Buttons have function + scrollIntoView functionality. Now to flesh it out and make some minor CSS changes so that the segments are the right size when scrolling on button click.

---
+++ public/imageCredits.md
@@ -0,0 +1,3 @@
+<a target="_blank" href="https://icons8.com/icon/106562/github">GitHub</a> icon by <a target="_blank" href="https://icons8.com">Icons8</a>
+
+<a target="_blank" href="https://icons8.com/icon/8808/linkedin">LinkedIn</a> icon by <a target="_blank" href="https://icons8.com">Icons8</a>
--- src/App.js
+++ src/App.js
@@ -1,22 +1,43 @@
-import React from 'react'
+import React, { useRef } from 'react'
 import avatar from "./avatar.png"
 import './App.css'
 import githubCat from "./githubCat.png"
 import linkedinIcon from "./linkedinIcon.png"
 
 export default function App() {
+  const segment1Ref = useRef(null);
+  const segment2Ref = useRef(null);
+  const segment3Ref = useRef(null);
+  const segment4Ref = useRef(null);
+
+  function handleHomeBtnClick() {
+    segment1Ref.current.scrollIntoView({ behavior: 'smooth'});
+  };
+
+  function handleAboutBtnClick() {
+    segment2Ref.current.scrollIntoView({ behavior: 'smooth'});
+  };
+
+  function handlePortfolioBtnClick() {
+    segment3Ref.current.scrollIntoView({ behavior: 'smooth'});
+  };
+
+  function handleContactBtnClick() {
+    segment4Ref.current.scrollIntoView({ behavior: 'smooth'});
+  };
+
   return (
     <>
     <div className='NavBar'>
       <div className='NavBarButtons'>
-      <button>Home</button>
-      <button>About</button>
-      <button>Portfolio</button>
-      <button>Contact Me</button>
+      <button onClick={handleHomeBtnClick}>Home</button>
+      <button onClick={handleAboutBtnClick}>About</button>
+      <button onClick={handlePortfolioBtnClick}>Portfolio</button>
+      <button onClick={handleContactBtnClick}>Contact Me</button>
       </div>
     </div>
 
-    <div className='Segment1'>
+    <div className='Segment1' ref={segment1Ref}>
         <div className='TextContainer'>
     <h1>Hi, I'm Deen.</h1>
     <h3>...with 2 e's.</h3>
@@ -32,6 +53,9 @@ export default function App() {
         <img src={linkedinIcon}
         className='linkedinIcon' alt='LinkedIn Icon'/>
         </a>
+        <h3>Pssst, these are clickable</h3>
+        <br/>
+        <h3>Promise I'll add some animations soon...</h3>
       </div> {/*End of IconContainer*/}
 
     </div> {/*End of TextContainer*/}
@@ -42,11 +66,25 @@ export default function App() {
     </div> {/*End of Segment1 div*/}
 
 
-    <div className='Segment2'>
+    <div className='Segment2' ref={segment2Ref}>
       <div className='TextContainer'>
         <h1>About me</h1>
         <h3>Lorem Ipsum</h3>
         <h3>*Insert Icons for Webstack*</h3>
+      </div>
+    </div>
+
+    <div className='Segment3' ref={segment3Ref}>
+      <div className='TextContainer'>
+        <h1>Portfolio</h1>
+        <h3>Lorem Ipsum</h3>
+      </div>
+    </div>
+
+    <div className='Segment4' ref={segment4Ref}>
+      <div className='TextContainer'>
+        <h1>Contact Me</h1>
+        <h3>Feel free to reach out on LinkedIn :&#41;</h3>
       </div>
     </div>
     </>
